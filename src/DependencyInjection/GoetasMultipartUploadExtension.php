841bb520ebb3d4f26034e56c41c09200f6b1b300
Asmir Mustafic <goetas@gmail.com>
2018-12-10 17:42:04 +0100

    use lower case service and param names

--- src/DependencyInjection/GoetasMultipartUploadExtension.php
+++ src/DependencyInjection/GoetasMultipartUploadExtension.php
@@ -17,13 +17,13 @@ class GoetasMultipartUploadExtension extends Extension
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
 
-        if ($container->hasParameter('Goetas.multipart_upload.stream_temp_dir')) {
-            $tempDir = $container->getParameter('Goetas.multipart_upload.stream_temp_dir');
+        if ($container->hasParameter('goetas.multipart_upload.stream_temp_dir') && $container->getParameter('goetas.multipart_upload.stream_temp_dir')) {
+            $tempDir = $container->getParameter('goetas.multipart_upload.stream_temp_dir');
         } else {
             $tempDir = ini_get('upload_tmp_dir') ?: sys_get_temp_dir();
         }
 
-        $def = $container->getDefinition('Goetas.multipart_upload.request_listener');
+        $def = $container->getDefinition('goetas.multipart_upload.request_listener');
         $def->setArgument(0, $tempDir);
     }
 }
