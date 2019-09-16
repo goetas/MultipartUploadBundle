1648a8930c9228706e1f8117b4ee0e481e8d2cbb
Tobias Nyholm <tobias.nyholm@gmail.com>
2019-09-16 15:52:55 +0200

    Some code style

--- src/EventListener/MultipartRequestListener.php
+++ src/EventListener/MultipartRequestListener.php
@@ -40,75 +40,76 @@ class MultipartRequestListener
     private function processRequest(Request $request)
     {
         $contentType = $request->headers->get('Content-Type');
-        if (0 === strpos($contentType, 'multipart/') && false === strpos($contentType, 'multipart/form-data')) {
-
-            $streamedPart = HttpFoundation::convert($request);
-
-            if ($this->injectFirstPart === true) {
-                $request->headers->remove('Content-Type');
-                $request->headers->remove('Content-Length');
-            }
-            $attachments = [];
-            $relatedParts = $streamedPart->getParts();
-
-            foreach ($relatedParts as $k => $part) {
-
-                if ($this->injectFirstPart === true && 0 === $k) {
-                    $request->headers->add($part->getHeaders());
-                    if ('application/x-www-form-urlencoded' === $part->getHeader('Content-Type')) {
-                        $output = [];
-                        parse_str($part->getBody(), $output);
-                        $request->request->add($output);
-                    } else {
-                        $this->setRequestContent($request, $part->getBody());
-                    }
-                    continue;
-                }
-
-                $contentDisposition = $part->getHeader('Content-Disposition');
-
-                if ($contentDisposition === null) {
-                    continue;
-                }
-
-                $fileName = $part->getFileName();
-
-                if ($fileName!== null) {
-                    $fp = tmpfile();
-                    fwrite($fp, $part->getBody());
-                    rewind($fp);
-
-                    $tmpPath = stream_get_meta_data($fp)['uri'];
-
-                    $ref = new \ReflectionClass('Symfony\Component\HttpFoundation\File\UploadedFile');
-                    $params = $ref->getConstructor()->getParameters();
-                    if ('error' === $params[3]->getName()) { // symfony 4
-                        $file = new UploadedFile($tmpPath, urldecode($fileName), $part->getMimeType(), null, true);
-                    } else { // symfony < 4
-                        $file = new UploadedFile($tmpPath, urldecode($fileName), $part->getMimeType(), filesize($tmpPath), null, true);
-                    }
-                    @$file->ref = $fp;
-                    $attachments[] = $file;
-                }
-
-                if (($formName = $this->isDispositionFormData($contentDisposition)) !== null) {
-                    $formPath = $this->parseKey($formName);
-
-                    if ($fileName !== null) {
-                        $files = $request->files->all();
-                        $files = $this->mergeFormArray($files, $formPath, $file);
-                        $request->files->replace($files);
-                    } else {
-                        $data = $request->request->all();
-                        $data = $this->mergeFormArray($data, $formPath, $part->getBody());
-                        $request->request->replace($data);
-                    }
-                }
-            }
-
-            $request->attributes->set('attachments', $attachments);
-            $request->attributes->set('related-parts', $relatedParts);
+        if (0 !== strpos($contentType, 'multipart/') || false !== strpos($contentType, 'multipart/form-data')) {
+            return;
         }
+
+        $streamedPart = HttpFoundation::convert($request);
+
+        if ($this->injectFirstPart === true) {
+            $request->headers->remove('Content-Type');
+            $request->headers->remove('Content-Length');
+        }
+        $attachments = [];
+        $relatedParts = $streamedPart->getParts();
+
+        foreach ($relatedParts as $k => $part) {
+
+            if ($this->injectFirstPart === true && 0 === $k) {
+                $request->headers->add($part->getHeaders());
+                if ('application/x-www-form-urlencoded' === $part->getHeader('Content-Type')) {
+                    $output = [];
+                    parse_str($part->getBody(), $output);
+                    $request->request->add($output);
+                } else {
+                    $this->setRequestContent($request, $part->getBody());
+                }
+                continue;
+            }
+
+            $contentDisposition = $part->getHeader('Content-Disposition');
+
+            if ($contentDisposition === null) {
+                continue;
+            }
+
+            $fileName = $part->getFileName();
+
+            if ($fileName!== null) {
+                $fp = tmpfile();
+                fwrite($fp, $part->getBody());
+                rewind($fp);
+
+                $tmpPath = stream_get_meta_data($fp)['uri'];
+
+                $ref = new \ReflectionClass('Symfony\Component\HttpFoundation\File\UploadedFile');
+                $params = $ref->getConstructor()->getParameters();
+                if ('error' === $params[3]->getName()) { // symfony 4
+                    $file = new UploadedFile($tmpPath, urldecode($fileName), $part->getMimeType(), null, true);
+                } else { // symfony < 4
+                    $file = new UploadedFile($tmpPath, urldecode($fileName), $part->getMimeType(), filesize($tmpPath), null, true);
+                }
+                @$file->ref = $fp;
+                $attachments[] = $file;
+            }
+
+            if (($formName = $this->isDispositionFormData($contentDisposition)) !== null) {
+                $formPath = $this->parseKey($formName);
+
+                if ($fileName !== null) {
+                    $files = $request->files->all();
+                    $files = $this->mergeFormArray($files, $formPath, $file);
+                    $request->files->replace($files);
+                } else {
+                    $data = $request->request->all();
+                    $data = $this->mergeFormArray($data, $formPath, $part->getBody());
+                    $request->request->replace($data);
+                }
+            }
+        }
+
+        $request->attributes->set('attachments', $attachments);
+        $request->attributes->set('related-parts', $relatedParts);
     }
 
     protected function mergeFormArray($array, $path, $data)
